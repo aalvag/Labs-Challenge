8083cde573dddd3115965cc049b85e866766a45f
Alejandro David Alvarez <48391594+aalvag@users.noreply.github.com>
2021-02-05 03:48:20 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,6 @@ app.use(express.json());
 app.use(cors());
 
 const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')));
